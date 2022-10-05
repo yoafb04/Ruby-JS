d03c7ddc7371c4104cbc72921eb9f1663e17b894
Yoannes <yoafb04@yahoo.com>
2022-10-04 22:30:14 -0400

    Update hello.rb

--- hello.rb
+++ hello.rb
@@ -3,22 +3,30 @@ def my_function
    headers = ['"Autor":      ', '"Publisher":      ', '"URL":      ']
    mylines = []
    i = 0
-   puts ("{\r\n")
-   puts ('   "Citation: "   {')
+
+   # Creating aand writing into a file
+   fileobject = File.new("JSFILE.json", "w+");
+   fileobject.syswrite("{\r\n");	
+   fileobject.syswrite('   "Citation: "   {' + "\r\n");	
+
 
    File.readlines('test.txt').each do |line|
       mylines << line
   end
 
   for b in headers do
-   puts "      #{b} " + '"' + mylines[i] 
+   str ="      #{b} " + '"' + mylines[i]  
+   fileobject.syswrite(str.chop + '"' + "," + "\r\n");	
+
    i = i + 1
   end
 
-  puts ("     }\r\n")
-  puts("}")
+  fileobject.syswrite("\r\n  }\r\n");
+  fileobject.syswrite("}");
 
 end
 
  @result = my_function
+
+
 
